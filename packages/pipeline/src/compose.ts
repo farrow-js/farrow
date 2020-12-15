53eea66b5c9e18fad3a019966615765cc2811ada
yj_gu <yj_gu@ctrip.com>
2020-12-15 16:03:29 +0800

    feat(pipeline): add koa-compose features

---
+++ packages/pipeline/src/compose.ts
@@ -0,0 +1,39 @@
+import { createPipeline, Middleware } from './pipeline'
+
+export type MiddlewareReturnType<T> = void | undefined | T | Promise<T | void | undefined>
+
+export type KoaMiddleware<T, U = void> = Middleware<T, MiddlewareReturnType<U>>
+
+export const compose = <T, U>(middlewares: KoaMiddleware<T, U>[]) => {
+  if (!Array.isArray(middlewares)) {
+    throw new TypeError('Middleware stack must be an array!')
+  }
+
+  for (const fn of middlewares) {
+    if (typeof fn !== 'function') {
+      throw new TypeError('Middleware must be composed of functions!')
+    }
+  }
+
+  return async (context: T, next?: KoaMiddleware<T, U>) => {
+    let pipeline = createPipeline<T, MiddlewareReturnType<U>>()
+
+    pipeline.use(...middlewares.map(toKoaMiddleware))
+
+    if (next) pipeline.use(next)
+
+    return pipeline.run(context, {
+      onLast: async () => {},
+    })
+  }
+}
+
+const toKoaMiddleware = <T, U>(middleware: KoaMiddleware<T, U>): typeof middleware => {
+  return (context, next) => {
+    let count = 0
+    return middleware(context, async (context) => {
+      if (count++ > 0) throw new Error('next() called multiple times')
+      return next(context)
+    })
+  }
+}

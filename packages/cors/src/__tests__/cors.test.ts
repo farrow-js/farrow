f21b030f5828608781ba924929485630bdee6cdf
工业聚 <guyingjie129@163.com>
2020-12-25 10:24:20 +0800

    feat(farrow-cors): make farrow-cors public

---
+++ packages/cors/src/__tests__/cors.test.ts
@@ -0,0 +1,43 @@
+import request from 'supertest'
+import { Http, HttpPipelineOptions, Response } from 'farrow-http'
+import { cors } from '../index'
+
+const createHttp = (options?: HttpPipelineOptions) => {
+  return Http({
+    logger: false,
+    ...options,
+  })
+}
+
+describe('farrow-cors', () => {
+  it('supports static options', async () => {
+    let http = createHttp()
+    let server = http.server()
+
+    http.use(
+      cors({
+        origin: 'http://example.com',
+        methods: ['FOO', 'bar'],
+        headers: ['FIZZ', 'buzz'],
+        credentials: true,
+        maxAge: 123,
+      }),
+    )
+
+    http.post('/test').use((request) => {
+      return Response.json(request)
+    })
+
+    await request(server)
+      .options('/test')
+      .set({
+        origin: 'http://example.com',
+        'access-control-request-headers': 'x-header-1, x-header-2',
+      })
+      .expect('Access-Control-Allow-Origin', 'http://example.com')
+      .expect('Access-Control-Allow-Methods', 'FOO,bar')
+      .expect('Access-Control-Allow-Headers', 'FIZZ,buzz')
+      .expect('Access-Control-Allow-Credentials', 'true')
+      .expect('Access-Control-Max-Age', '123')
+  })
+})
--- packages/cors/src/index.ts
+++ packages/cors/src/index.ts
@@ -1,5 +1,3 @@
-// the code is based on https://github.com/expressjs/cors/blob/master/lib/index.js
-
 import { Middleware } from 'farrow-pipeline'
 import { Response, useReq, useRes, MaybeAsyncResponse } from 'farrow-http'
 import { promisify } from 'util'
@@ -24,7 +22,16 @@ export const cors = <T extends CorsRequest = CorsRequest>(
 
     try {
       await cors(req, res)
-      return next(request)
+      if (req.method?.toLowerCase() === 'options') {
+        if (typeof options === 'object') {
+          if (options.preflightContinue) {
+            return next(request)
+          }
+        }
+        return Response.status(204).string('')
+      } else {
+        return next(request)
+      }
     } catch (error) {
       return Response.status(500).text(error.message)
     }

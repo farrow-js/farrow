d884d084e63e772659f79422f301749bd35461b2
Jade Gu <guyingjie129@163.com>
2021-03-02 18:31:56 +0800

    feat(next): module supports BaseModule and ModuleConfigKey

--- packages/next/src/module.ts
+++ packages/next/src/module.ts
@@ -13,6 +13,8 @@ export type ModuleContext = {
 
 const ModuleContextKey = Symbol('module.context.key')
 
+export const ModuleConfigKey = Symbol('module.config.key')
+
 let disableContructorCall = true
 
 export const newModule = <T extends Module>(Ctor: ModuleCtor<T>, ctx: ModuleContext): T => {
@@ -22,9 +24,7 @@ export const newModule = <T extends Module>(Ctor: ModuleCtor<T>, ctx: ModuleCont
   return module
 }
 
-export const getModule = <T extends Module>(module: Module, Ctor: ModuleCtor<T>): T => {
-  let ctx = module[ModuleContextKey]
-
+export const getModule = <T extends Module>(Ctor: ModuleCtor<T>, ctx: ModuleContext): T => {
   if (ctx.modules.has(Ctor)) {
     return ctx.modules.get(Ctor)! as T
   }
@@ -36,9 +36,7 @@ export const getModule = <T extends Module>(module: Module, Ctor: ModuleCtor<T>)
   return m as T
 }
 
-export const getModuleConfig = <T extends ModuleConfig>(module: Module, Ctor: ModuleConfigCtor<T>): T => {
-  let ctx = module[ModuleContextKey]
-
+export const getModuleConfig = <T extends ModuleConfig>(Ctor: ModuleConfigCtor<T>, ctx: ModuleContext): T => {
   if (ctx.configs.has(Ctor)) {
     return ctx.configs.get(Ctor)! as T
   }
@@ -51,39 +49,40 @@ export const createModuleContext = (): ModuleContext => ({
   configs: new WeakMap(),
 })
 
+const attachModuleConfig = (ctx: ModuleContext, configs: ModuleConfig[]) => {
+  for (let config of configs) {
+    let ConfigCtor = config.constructor as ModuleConfigCtor
+    let hasConfig = ctx.configs.has(ConfigCtor)
+    if (!hasConfig) {
+      ctx.configs.set(ConfigCtor, config)
+    }
+  }
+}
+
 export const initilize = <T extends Module>(
   Ctor: ModuleCtor<T>,
   moduleConfigs: ModuleConfig[],
   ctx: ModuleContext = createModuleContext(),
 ) => {
-  let { modules, configs } = ctx
+  attachModuleConfig(ctx, moduleConfigs)
 
-  moduleConfigs.forEach((moduleConfig) => {
-    configs.set(moduleConfig.constructor as ModuleConfigCtor, moduleConfig)
-  })
-
-  if (modules.has(Ctor)) {
-    return modules.get(Ctor)! as T
+  if (ctx.modules.has(Ctor)) {
+    return ctx.modules.get(Ctor)! as T
   }
 
-  let module = newModule(Ctor, { modules, configs })
+  let module = newModule(Ctor, ctx)
 
-  modules.set(Ctor, module)
+  ctx.modules.set(Ctor, module)
 
   return module
 }
 
 export abstract class ModuleConfig {}
 
-export abstract class Module {
-  [ModuleContextKey]: ModuleContext
+export abstract class BaseModule {
+  [ModuleConfigKey]?: ModuleConfig[];
 
-  constructor(ctx: ModuleContext) {
-    if (disableContructorCall) {
-      throw new Error('Should not new Module() manually, using this.using(ModuleCtor) instead')
-    }
-    this[ModuleContextKey] = ctx
-  }
+  [ModuleContextKey] = createModuleContext()
 
   /**
    *
@@ -94,13 +93,32 @@ export abstract class Module {
   use<T extends ModuleConfig>(Ctor: new (...args: any) => T): T
 
   use(Ctor: new (...args: any) => any) {
+    let configs = this[ModuleConfigKey]
+    let ctx = this[ModuleContextKey]
+
+    if (configs) {
+      attachModuleConfig(ctx, configs)
+    }
+
     if (Ctor.prototype instanceof ModuleConfig) {
-      return getModuleConfig(this, Ctor)
+      return getModuleConfig(Ctor, ctx)
     }
+
     if (Ctor.prototype instanceof Module) {
-      return getModule(this, Ctor)
+      return getModule(Ctor, ctx)
     }
+
     throw new Error(`Unsupported Constructor: ${Ctor}`)
+  }
+}
+
+export abstract class Module extends BaseModule {
+  constructor(ctx: ModuleContext) {
+    super()
+    if (disableContructorCall) {
+      throw new Error('Should not new Module() manually, using this.using(ModuleCtor) instead')
+    }
+    this[ModuleContextKey] = ctx
   }
 }
 
@@ -149,13 +167,24 @@ export abstract class Module {
 //   }
 // }
 
+// class App extends BaseModule {
+//   [ModuleConfigKey] = [new PageInfo('/path/for/test', 'test')]
+//   root = this.use(Root)
+// }
+
+// let app = new App()
 // let root = initilize(Root, [new PageInfo('/path/for/test', 'test')])
 
-// console.log('info', {
-//   info: root.self.self.self.getInfo(),
-//   [`root is equal to root.self`]: root === root.self,
-//   [`user.page is equal to product.page`]: root.user.page === root.product.page,
-//   [`user.product is euqal to product`]: root.user.product === root.product,
-//   [`product.user is equal to user`]: root.product.user === root.user,
-//   [`product.user.product is equal to product`]: root.product.user.product === root.product
-// })
+// let log = (root: Root, tag = 'info') => {
+//   console.log(tag, {
+//     info: root.self.self.self.getInfo(),
+//     [`root is equal to root.self`]: root === root.self,
+//     [`user.page is equal to product.page`]: root.user.page === root.product.page,
+//     [`user.product is euqal to product`]: root.user.product === root.product,
+//     [`product.user is equal to user`]: root.product.user === root.user,
+//     [`product.user.product is equal to product`]: root.product.user.product === root.product,
+//   })
+// }
+
+// log(app.root, 'app-info')
+// log(root)

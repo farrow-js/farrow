9dac58efa92438347f13de8dd5918884a2363eeb
sinoon <sinoon1218@gmail.com>
2021-03-14 12:27:02 +0800

    fix cors not support function option

--- packages/farrow-cors/src/__tests__/cors.test.ts
+++ packages/farrow-cors/src/__tests__/cors.test.ts
@@ -40,4 +40,37 @@ describe('farrow-cors', () => {
       .expect('Access-Control-Allow-Credentials', 'true')
       .expect('Access-Control-Max-Age', '123')
   })
+
+  it('should support function option', async () => {
+    let http = createHttp()
+    let server = http.server()
+
+    http.use(
+      cors((request, callback) => {
+        callback(null, {
+          origin: request.headers['origin'],
+          methods: ['FOO', 'bar'],
+          headers: ['FIZZ', 'buzz'],
+          credentials: true,
+          maxAge: 123,
+        })
+      }),
+    )
+
+    http.post('/test').use((request) => {
+      return Response.json(request)
+    })
+
+    await request(server)
+      .options('/test')
+      .set({
+        origin: 'http://example.com',
+        'access-control-request-headers': 'x-header-1, x-header-2',
+      })
+      .expect('Access-Control-Allow-Origin', 'http://example.com')
+      .expect('Access-Control-Allow-Methods', 'FOO,bar')
+      .expect('Access-Control-Allow-Headers', 'FIZZ,buzz')
+      .expect('Access-Control-Allow-Credentials', 'true')
+      .expect('Access-Control-Max-Age', '123')
+  })
 })

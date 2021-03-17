840cf17defa0c956636aa3f7ad58cb3ce7148fef
Jade Gu <guyingjie129@163.com>
2021-03-17 17:36:30 +0800

    fix: mark all template projects private

--- packages/create-farrow-app/templates/farrow-vite-react/server/index.ts
+++ packages/create-farrow-app/templates/farrow-vite-react/server/index.ts
@@ -4,16 +4,22 @@ import { vite } from 'farrow-vite'
 
 import { services } from './api'
 
-let http = Http()
+// create http server
+const http = Http()
 
+// attach service for api
 http.use(services)
 
+// attach vite or assets for page
 if (process.env.NODE_ENV === 'development') {
+  // enable vite-dev-server when development
   http.use(vite())
 } else {
+  // enable vite-bundle-output when production
   http.serve('/', path.join(__dirname, '../dist/client'))
 }
 
+// start listening
 http.listen(3003, () => {
   console.log('server started at http://localhost:3002')
 })

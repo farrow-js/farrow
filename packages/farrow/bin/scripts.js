f82cf77176752863b65adfc11a4a054ad98d58f6
zuozijian <zuozijian1994@gmail.com>
2021-05-22 21:58:45 +0800

    refactor let to const

--- packages/farrow/bin/scripts.js
+++ packages/farrow/bin/scripts.js
@@ -21,7 +21,7 @@ const script = scriptIndex === -1 ? args[0] : args[scriptIndex]
 const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : []
 
 if (scripts.includes(script)) {
-  let result = spawn.sync(
+  const result = spawn.sync(
     process.execPath,
     nodeArgs.concat(require.resolve(`../scripts/${script}`)).concat(args.slice(scriptIndex + 1)),
     { stdio: 'inherit' },

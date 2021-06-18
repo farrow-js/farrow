bde015444e33ed0a658fe6643f92f44025b6c052
马天琦 <matianqi.tqma@bytedance.com>
2021-06-18 20:35:46 +0800

    fix(farrow-api-express): hook add detect process

--- packages/farrow-api-express/src/context.ts
+++ packages/farrow-api-express/src/context.ts
@@ -5,6 +5,11 @@ export const RequestContext = createContext<Request | null>(null)
 
 export const useRequest = () => {
   const request = RequestContext.use().value
+
+  if (!request) {
+    throw new Error(`Can't call useRequest out of scope, it should be placed on top of the function`)
+  }
+
   return request
 }
 
@@ -13,13 +18,21 @@ export const ResponseContext = createContext<Response | null>(null)
 export const useResponse = () => {
   const response = ResponseContext.use().value
 
+  if (!response) {
+    throw new Error(`Can't call useResponse out of scope, it should be placed on top of the function`)
+  }
+
   return response
 }
 
 export const NextContext = createContext<NextFunction | null>(null)
 
 export const useNext = () => {
-  const response = NextContext.use().value
+  const next = NextContext.use().value
 
-  return response
+  if (!next) {
+    throw new Error(`Can't call useNext out of scope, it should be placed on top of the function`)
+  }
+
+  return next
 }

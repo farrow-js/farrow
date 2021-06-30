b3ab51a398f645314356bf460d5756a8bb0749fc
Jade <guyingjie129@163.com>
2021-06-30 16:17:47 +0800

    chore: update

---
+++ examples/next-app/pages/404.tsx
@@ -0,0 +1,1 @@
+export { default } from '../src/pages/404'
---
+++ examples/next-app/src/pages/404/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function NotFound() {
+  return (
+    <>
+      <h1>404</h1>
+      <Link href="/">back to home</Link>
+    </>
+  )
+}

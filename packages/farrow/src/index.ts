0a14c2f235906888fae82d33b1d63dbd2e5941dd
tqma113 <mtq1997@126.com>
2021-12-09 15:59:57 +0800

    feat(farrow): defineConfig (#145)
    * feat(farrow): defineConfig
    
    * docs(changeset): Feat defineConfig

--- packages/farrow/src/index.ts
+++ packages/farrow/src/index.ts
@@ -1,5 +1,12 @@
 import { Config } from './config'
 
+export { Config }
+
+export const defineConfig = (config: Config) => {
+  return config
+}
+
+// compatibility
 export const createFarrowConfig = (config: Config) => {
   return config
 }

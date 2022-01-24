ca40c15be12a8b4ce013bb3ccb1e4f72bf6a7284
matianqi.tqma <matianqi.tqma@bytedance.com>
2022-01-24 13:51:36 +0800

    fix: lock file and type error

--- packages/farrow-deno-api/__tests__/denoService.test.ts
+++ packages/farrow-deno-api/__tests__/denoService.test.ts
@@ -89,8 +89,6 @@ describe('deno-api', () => {
   })
 
   it('should work', async () => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
     global.fetch = fetch as any
     const http = createHttp()
 
